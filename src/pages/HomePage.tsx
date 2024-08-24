18611b3225966427d9ce38711d1ad7e9f50b4f10
Robin <robinpersson94@hotmail.com>
2024-08-24 15:09:16 +0200

    Security commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,19 @@
-import { Button, Form, InputGroup } from "react-bootstrap"
+import { useState } from "react";
+import { Button, Form, Image, InputGroup } from "react-bootstrap";
 
 const HomePage = () => {
+  const [input, setInput] = useState<string>("");
+
+  const handleSearch = () => {
+
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInput(value);
+    console.log(input)
+  };
+
   return (
     <>
       <InputGroup className="mb-3" id="searchField">
@@ -8,13 +21,16 @@ const HomePage = () => {
           placeholder="What do you want to eat?"
           aria-label="What do you want to eat?"
           aria-describedby="basic-addon2"
+          value={input}
+          onChange={handleChange}
         />
-        <Button variant="outline-secondary" id="button-addon">
+        <Button variant="outline-secondary" id="button-addon" onClick={handleSearch}>
           Search
         </Button>
       </InputGroup>
+      <Image src="https://placehold.co/400" />
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
