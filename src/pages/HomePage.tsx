be0fc0f4b1a2fe0dbd8926197972be03e161b146
Robin <robinpersson94@hotmail.com>
2024-08-24 15:39:11 +0200

    Added map-component to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,39 +1,40 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import { Button, Form, Image, InputGroup } from "react-bootstrap";
-=======
->>>>>>> main
+import GoogleMapp from "../components/GoogleMap";
 
 const HomePage = () => {
-  const [input, setInput] = useState<string>("");
+    const [input, setInput] = useState<string>("");
 
-  const handleSearch = () => {
+    const handleSearch = () => {
 
-  };
+    };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setInput(value);
-    console.log(input)
-  };
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setInput(value);
+        console.log(input)
+    };
 
-  return (
-    <>
-      <InputGroup className="mb-3" id="searchField">
-        <Form.Control
-          placeholder="What do you want to eat?"
-          aria-label="What do you want to eat?"
-          aria-describedby="basic-addon2"
-          value={input}
-          onChange={handleChange}
-        />
-        <Button variant="outline-secondary" id="button-addon" onClick={handleSearch}>
-          Search
-        </Button>
-      </InputGroup>
-      <Image src="https://placehold.co/400" />
-    </>
-  );
+    return (
+        <>
+            <InputGroup className="mb-3" id="searchField">
+                <Form.Control
+                    placeholder="What do you want to eat?"
+                    aria-label="What do you want to eat?"
+                    aria-describedby="basic-addon2"
+                    value={input}
+                    onChange={handleChange}
+                />
+                <Button variant="outline-secondary" id="button-addon" onClick={handleSearch}>
+                    Search
+                </Button>
+            </InputGroup>
+            {/* <Image src="https://placehold.co/400" /> */}
+            <GoogleMapp />
+            //Add component for rendering lists
+
+        </>
+    );
 }
 
 export default HomePage;
