51891ffbd3b9fd5359d95888feee43db3b27ac58
Tomasz Polrolniczak <tomek.polrolniczak@gmail.com>
2024-10-04 09:57:48 +0200

    Add a new drop down item in admin's menu and make it show above the page Instead of causing an overflow of the upper part

--- src/components/NavBarAdmin.tsx
+++ src/components/NavBarAdmin.tsx
@@ -1,4 +1,3 @@
-import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { NavDropdown } from "react-bootstrap";
@@ -7,7 +6,6 @@ import "../assets/NavBar.scss";
 function NavBarAdmin() {
   return (
     <Navbar expand="md" className="bg-body-tertiary" id="custom-navbar">
-      <Container>
         <Navbar.Brand href="/" id="navHead">
           Pizza Hunters AB 🍕
         </Navbar.Brand>
@@ -18,12 +16,12 @@ function NavBarAdmin() {
             <Nav.Link href="/suggestions">Suggest a new restaurant for us!</Nav.Link>
             <NavDropdown title="Admin Paths" id="basic-nav-dropdown">
               <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+              <NavDropdown.Item href="/places">Restaurants</NavDropdown.Item>
               <NavDropdown.Item href="/advice">Users Suggestions</NavDropdown.Item>
               <NavDropdown.Item href="/logout">Sign out?</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
-      </Container>
     </Navbar>
   );
 }
